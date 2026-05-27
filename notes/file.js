e64aeccb1300148c24a127af894d115521b558b9
samikshabansal7 <samikshabansal990@gmail.com>
2026-05-27 12:30:15 +0530

    Add files via upload

--- notes/file.js
+++ notes/file.js
@@ -1,1 +1,21 @@
+ const fs=require("fs")
 
+fs.writeFileSync("./test.txt","hey hii")
+
+const result=fs.readFileSync("./contacts.txt","utf-8")
+console.log(result);
+fs.readFile("./contacts.txt","utf-8",(err,result) =>{
+    if(err){
+        console.log("error",err)
+    }
+    else{
+        console.log(result)
+    }
+})
+
+fs.appendFileSync("./test.txt",new Date().getDate().toLocaleString())
+fs.appendFileSync("./test.txt",`${Date.now()}hey there\n`)
+fs.appendFileSync("./test.txt",`${Date.now()} hey there \n `)
+fs.unlinkSync("./copy.txt")//command use for delete 
+console.log(fs.statSync("./test.txt"))
+fs.mkdir("my-docs")
